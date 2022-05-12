310326edc10049972b290bd725ffad44531520a5
Rushabh862001 <105192713+Rushabh862001@users.noreply.github.com>
2022-05-12 12:16:37 +0530

    Add files via upload

---
+++ hands-on/Arithmetic.java
@@ -0,0 +1,24 @@
+package corejava;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Num1, Num2, result;
+		Num1 = 7000;
+		Num2 = 9000;
+		
+		int sum = Num1 + Num2;
+		int sub = Num1 - Num2;
+		int mul = Num1 * Num2;
+		int div = Num1 / Num2;
+		int mod = Num1 % Num2;
+        
+        System.out.println("sum:" + sum);
+        System.out.println("sub:" + sub);
+        System.out.println("mul:" + mul);
+        System.out.println("div:" + div);
+        System.out.println("mod:" + mod); 
+	}
+
+}
