7f54bff78fc659ef1f3303870a1a716a9c012e6a
Rushabh862001 <105192713+Rushabh862001@users.noreply.github.com>
2022-05-12 12:54:25 +0530

    Add files via upload

---
+++ hands-on/Explicit.java
@@ -0,0 +1,20 @@
+package corejava;
+
+public class Explicit {
+
+	public static void main(String[] args) {
+		
+		double d = 40.025;
+		 
+		//explicit type casting
+		int i =(int)d;
+		
+		System.out.println("Double Data type value "+d);
+		 
+		//fractional part lost
+		System.out.println("Int Data type value "+i);
+
+
+	}
+
+}
---
+++ hands-on/Implicit.java
@@ -0,0 +1,17 @@
+package corejava;
+
+public class Implicit {
+
+	public static void main(String[] args) {
+		
+		int i=30;
+		
+		//automatic type conversion
+		double d = i;
+		
+		System.out.println("Int value "+i);
+		System.out.println("double value "+d);
+
+	}
+
+}
