57cc053fb90be553037c5f51db98a185e3b50828
Rushabh862001 <105192713+Rushabh862001@users.noreply.github.com>
2022-06-03 12:20:53 +0530

    Add files via upload

---
+++ Practical/BoxingUnboxing.java
@@ -0,0 +1,20 @@
+package corejava;
+
+public class BoxingUnboxing {
+
+	public static void main(String[] args) {
+		
+		int i=5;
+		Integer b= new Integer(i);
+		
+		System.out.println(b);
+		Byte a= 105;
+		
+        byte b1=a;
+        System.out.println(a);
+        
+        
+        
+	}
+
+}
